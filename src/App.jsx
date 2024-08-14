ec0a656e08d54345e189e073ee574fc098d7b81b
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-08-14 12:39:12 +0530

    added hot chocolate comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <p style={{ display: 'flex', justifyContent: 'center', fontWeight: "800", alignItems: 'center', gap: "10px", fontSize:"25px" }} className='bungee-tint-regular'>Arizona - India Time Calculator  <picture>
-          <source srcset="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.webp" type="image/webp" />
-          <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.gif" alt="😉" width="32" height="32" />
-        </picture></p>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <p style={{ display: 'flex', justifyContent: 'center', fontWeight: "800", alignItems: 'center', gap: "10px", fontSize: "25px" }} className='bungee-tint-regular'>
+          Arizona - India Time Calculator
+          <picture>
+            <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.webp" type="image/webp" />
+            <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f609/512.gif" alt="😉" width="32" height="32" />
+          </picture>
+        </p>
         <TimeCalculator />
+        <div style={{ marginTop: 'auto', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: "5px" }}>
+          <p className='playwrite-pe' style={{ fontSize: "10px", display: 'flex', alignItems: 'center' }}>Every time you use this site, you need to give Nagendra an Hot Chocolate
+
+            <picture>
+              <source srcset="https://fonts.gstatic.com/s/e/notoemoji/latest/2615/512.webp" type="image/webp" />
+              <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/2615/512.gif" alt="☕" width="22" height="22" />
+            </picture>
+            .
+          </p>
+        </div>
       </div>
+
     </>
   )
 }
