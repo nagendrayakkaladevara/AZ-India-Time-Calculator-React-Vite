bf56b62a66e04b2980e1577467ce1b6a00335d9b
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-08-26 00:18:01 +0530

    added user visit count in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import TimeCalculator from './TimeCalculator'
 
 function App() {
+
 
   return (
     <>
@@ -15,7 +17,7 @@ function App() {
         </p>
         <TimeCalculator />
         <div style={{ marginTop: 'auto', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: "5px" }}>
-          <p className='playwrite-pe' style={{ fontSize: "10px", display: 'flex', alignItems: 'center' }}>Every time you use this site, you need to give Nagendra an Hot Chocolate
+          <p className='playwrite-pe' style={{ fontSize: "10px", display: 'flex', alignItems: 'center' }}>Every time you use this site, you need to give Nagendra a Hot Chocolate
 
             <picture>
               <source srcset="https://fonts.gstatic.com/s/e/notoemoji/latest/2615/512.webp" type="image/webp" />
@@ -24,6 +26,7 @@ function App() {
             .
           </p>
         </div>
+        <Counter />
       </div>
 
     </>
@@ -31,3 +34,46 @@ function App() {
 }
 
 export default App
+
+const Counter = () => {
+  const [visitCount, setVisitCount] = useState(0);
+  console.log("🚀 ~ Counter ~ visitCount:", visitCount)
+
+  useEffect(() => {
+    // Check if this is the first render by seeing if the flag is 'false'
+    let isFirstRender = localStorage.getItem('Arizona-India-Time-Calculator-firstRender') !== 'false';
+    console.log("🚀 ~ useEffect ~ isFirstRender:", isFirstRender);
+
+    if (isFirstRender) {
+      let count = localStorage.getItem('Arizona-India-Time-Calculator-visitCount');
+      console.log("🚀 ~ useEffect ~ count:", count);
+
+      if (!count) {
+        count = 1;
+      } else {
+        count = parseInt(count) + 1;
+      }
+
+      localStorage.setItem('Arizona-India-Time-Calculator-visitCount', count);
+      setVisitCount(count);
+
+
+      localStorage.setItem('Arizona-India-Time-Calculator-firstRender', 'false');
+    }
+
+
+    setTimeout(() => {
+      localStorage.setItem('Arizona-India-Time-Calculator-firstRender', 'true');
+    }, 1000);
+  }, []);
+
+
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: "center" }}>
+        <p className='playwrite-pe'>This is your visit number: {visitCount}</p>
+      </div>
+    </>
+  )
+}
